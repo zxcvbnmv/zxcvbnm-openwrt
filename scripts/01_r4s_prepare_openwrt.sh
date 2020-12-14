a3b732cb2ffc78a1025c32087c4fb14c4155e660
zxcvbnmv <40834572+zxcvbnmv@users.noreply.github.com>
2020-12-14 08:28:53 +0800

    Update 01_r4s_prepare_openwrt.sh

--- scripts/01_r4s_prepare_openwrt.sh
+++ scripts/01_r4s_prepare_openwrt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone -b openwrt-18.06-k5.4-nanopi-r4s --single-branch https://github.com/1715173329/openwrt
+git clone -b 18.06-k5.4-nanopi-r4s --single-branch https://github.com/1715173329/openwrt
 exit 0
