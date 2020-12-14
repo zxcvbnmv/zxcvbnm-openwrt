c9f3d62cf61f3f55f5a6e60816a2588a3a6ff684
zxcvbnmv <40834572+zxcvbnmv@users.noreply.github.com>
2020-12-15 04:08:37 +0800

    Update 01_r4s_prepare_openwrt.sh

--- scripts/01_r4s_prepare_openwrt.sh
+++ scripts/01_r4s_prepare_openwrt.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-git clone -b 1806-k54-nanopi-r4s --single-branch https://github.com/1715173329/openwrt
+git clone -b master https://git.openwrt.org/openwrt/openwrt.git openwrt
+git clone -b main --depth 1 https://github.com/Lienol/openwrt.git openwrt-lienol
+git clone -b main --depth 1 https://github.com/Lienol/openwrt-packages packages-lienol
+git clone -b main --depth 1 https://github.com/Lienol/openwrt-luci luci-lienol
+git clone -b linksys-ea6350v3-mastertrack --depth 1 https://github.com/NoTengoBattery/openwrt NoTengoBattery
 exit 0
