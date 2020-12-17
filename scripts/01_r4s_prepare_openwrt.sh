06c6fe59c28ee9dd8c3dd7c0b68a938f7d85ddb7
zxcvbnmv <40834572+zxcvbnmv@users.noreply.github.com>
2020-12-17 10:24:12 +0800

    Update 01_r4s_prepare_openwrt.sh

--- scripts/01_r4s_prepare_openwrt.sh
+++ scripts/01_r4s_prepare_openwrt.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 git clone -b master https://git.openwrt.org/openwrt/openwrt.git openwrt
+cd openwrt
+git checkout master
+git reset --hard a472791
+git checkout -b hooo a472791
+cd ../
 git clone -b main --depth 1 https://github.com/Lienol/openwrt.git openwrt-lienol
 git clone -b main --depth 1 https://github.com/Lienol/openwrt-packages packages-lienol
 git clone -b main --depth 1 https://github.com/Lienol/openwrt-luci luci-lienol
