21a352efa476c280b8e84511c539c05db823d6ff
zxcvbnmv <40834572+zxcvbnmv@users.noreply.github.com>
2020-12-25 14:09:11 +0800

    Update 01_r4s_prepare_openwrt.sh

--- scripts/01_r4s_prepare_openwrt.sh
+++ scripts/01_r4s_prepare_openwrt.sh
@@ -6,7 +6,4 @@ git reset --hard a472791
 git checkout -b hooo a472791
 cd ../
 git clone -b main --depth 1 https://github.com/Lienol/openwrt.git openwrt-lienol
-git clone -b main --depth 1 https://github.com/Lienol/openwrt-packages packages-lienol
-git clone -b main --depth 1 https://github.com/Lienol/openwrt-luci luci-lienol
-git clone -b linksys-ea6350v3-mastertrack --depth 1 https://github.com/NoTengoBattery/openwrt NoTengoBattery
 exit 0
