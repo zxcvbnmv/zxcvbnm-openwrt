1040bfa92d5667942561ad89c8fd58afc2c50b7f
zxcvbnmv <40834572+zxcvbnmv@users.noreply.github.com>
2021-01-26 23:46:21 +0800

    Update 01_r4s_prepare_openwrt.sh

--- scripts/01_r4s_prepare_openwrt.sh
+++ scripts/01_r4s_prepare_openwrt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git clone -b master https://git.openwrt.org/openwrt/openwrt.git openwrt
-cd openwrt
-git checkout master
-git reset --hard a472791
-git checkout -b hooo a472791
+#cd openwrt
+#git checkout master
+#git reset --hard a472791
+#git checkout -b hooo a472791
 exit 0
